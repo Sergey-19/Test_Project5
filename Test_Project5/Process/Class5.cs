e22a9395b779fd79fff6318fe82b6a14e0bc68d9
Minh <Sergeyen19@gmail.com>
2026-01-30 16:44:13 +0700

    Add class 5 to branch 1

---
+++ Test_Project5/Process/Class5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Project5.Process
+{
+    internal class Class5
+    {
+        public void say_Hi()
+        {
+            Console.WriteLine("Hi...");
+
+        }
+    }
+}
