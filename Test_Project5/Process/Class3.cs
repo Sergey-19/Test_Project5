20614683074d7b4552fbe375858048a668bbd17d
Minh <Sergeyen19@gmail.com>
2026-01-30 16:33:14 +0700

    Add class 3

---
+++ Test_Project5/Process/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Project5.Process
+{
+    internal class Class3
+    {
+        public void sayGoodBye()
+        {
+            Console.WriteLine("GoodBye");
+        }
+    }
+}
