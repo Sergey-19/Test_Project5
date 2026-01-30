6a3f9d1a6f14c50e4246f23efad53cb1416d8c01
Minh <Sergeyen19@gmail.com>
2026-01-30 17:01:26 +0700

    update class 5

--- Test_Project5/Process/Class5.cs
+++ Test_Project5/Process/Class5.cs
@@ -8,10 +8,15 @@ namespace Test_Project5.Process
 {
     internal class Class5
     {
-        public void say_Hi()
+        public void say_Hello_from_Class5()
         {
             Console.WriteLine("Hi...");
 
+        }
+
+        public void running2()
+        {
+            Console.WriteLine("running 2 is running...");
         }
     }
 }
